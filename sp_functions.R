212a7fb2b868a007f66ad05f0bceb580b00c2a24
amcarter <alicecarter@wl-10-191-34-76.wireless.duke.edu>
2017-08-11 14:59:30 -0400

    I made a change

--- sp_functions.R
+++ sp_functions.R
@@ -9,6 +9,8 @@
 #   return(x)
 # }
 
+alice_function <- function(){print('functions! yay!')}
+
 sp_data <- function(sitecode, startdate=NULL, enddate=NULL, variables=NULL, flags=FALSE, token=NULL){
     # sitecode is a site name
     # startdate and enddate are YYYY-MM-DD strings, e.g., '1983-12-09'
