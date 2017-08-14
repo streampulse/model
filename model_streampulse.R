4bc817b3951c3138a3f6b5b577b92ff86da7cff1
Aaron Berdanier <aaron.berdanier@gmail.com>
2017-08-14 16:39:29 -0400

    Big update

--- model_streampulse.R
+++ model_streampulse.R
@@ -13,37 +13,23 @@ library(tidyr)
 # In the future, turn these codes into a package and just source the package...
 sp_functions <- GET("https://raw.githubusercontent.com/streampulse/model/master/sp_functions.R")
 eval(parse(text = content(sp_functions, as="text", encoding="UTF-8")), envir= .GlobalEnv)
-gapfill_functions <- GET("https://raw.githubusercontent.com/streampulse/model/master/gapfill_functions.R")
-eval(parse(text = content(gapfill_functions, as="text", encoding="UTF-8")), envir= .GlobalEnv)
-BASE_functions <- GET("https://raw.githubusercontent.com/streampulse/model/master/BASE_functions.R")
-eval(parse(text = content(BASE_functions, as="text", encoding="UTF-8")), envir= .GlobalEnv)
 
 # Model type for streamMetabolizer
 # We recommend the Bayesian model, but you can also fit "mle", which runs much faster.
 model_type <- "bayes"
 # Which modeling framework to use
 # "streamMetabolizer" is default, can also use "BASE"
-model_name <- "BASE"
-
-# Get StreamPULSE data for metabolism modeling
-fitdata <- sp_data_metab(sitecode = "NC_Eno",
-    startdate = "2016-01-01", enddate = "2017-07-01",
-    type = model_type, model = model_name, fillgaps = TRUE)
-
-# Fit models
-predictions <- fit_metabolism(fitdata, model_name, model_type)
-
-#basepreds <- predict_BASE(tmp)
-
-
-
-# streamMetabolizer comparison
 model_name <- "streamMetabolizer"
 
-# Get StreamPULSE data for metabolism modeling
-smfitdata <- sp_data_metab(sitecode = "NC_Eno",
-    startdate = "2016-01-01", enddate = "2017-07-01",
+# Download data from streampulse and prepare for metabolism modeling
+fitdata <- prep_metabolism(sitecode = "NC_Eno",
+    startdate = "2016-01-01", enddate = "2017-01-01",
     type = model_type, model = model_name, fillgaps = TRUE)
 
-# Fit models
-smpredictions <- fit_metabolism(smfitdata, model_name, model_type)
+# Fit metabolism model
+#  - if using streamMetabolizer, returns a metab model object
+#  - if using BASE, returns a temporary directory where results are saved
+modelfit <- fit_metabolism(fitdata)
+
+# Gather metabolism predictions
+predictions <- predict_metabolism(modelfit)
