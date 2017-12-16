6edd684a3a2cdf032d174f14521f0d6bd5357c7a
Mike Vlah <vlahm13@gmail.com>
2017-12-15 22:37:29 -0800

    almost done testing outlier detector i think

--- ancillary/outlier_detection.R
+++ ancillary/outlier_detection.R
@@ -1,3 +1,5 @@
+#take one (ful series uploads) ####
+
 #sudo apt install libgsl0-dev
 # install.packages('tsoutliers')
 library(tsoutliers)
@@ -193,7 +195,24 @@ tsoutl = function(x, plot=FALSE)
 tsoutl(tm, TRUE) #ugh
 
 
-#winging it
+#winging it (four weeks) ####
+
+setwd('/home/mike/Dropbox/streampulse/data/NC_download/')
+site_files = dir()
+d = read.csv(site_files[1], stringsAsFactors=FALSE,
+    colClasses=c('DateTime_UTC'='POSIXct'))
+sitevars = unique(d$variable)
+d = d[d$variable == sitevars[1],]
+head(d)
+d = d[d$DateTime_UTC > as.POSIXct('2016-10-01 00:00:00') & d$DateTime_UTC <
+        as.POSIXct('2016-10-29 00:00:00'),]
+plot(d$value)
+
+library(imputeTS)
+tm = ts(d$value, deltat = 1/96)
+tm = na.seadec(tm, algorithm='interpolation')
+
+
 diffs = diff(tm)
 
 x11()
@@ -266,20 +285,290 @@ points(time(tm)[outlier_inds], diffs[outlier_inds], col='orange', pch=20, cex=2)
 # }
 
 if(length(outlier_inds) %% 2 == 0){
-    mapply(function(x, y), outlier_inds[seq(1, length(outlier_inds), 2)],
-        outlier_inds[seq(2, length(outlier_inds), 2)])
+    outlier_ts = mapply(function(x, y){ seq(x+1, y, 1) },
+        outlier_inds[seq(1, length(outlier_inds), 2)],
+        outlier_inds[seq(2, length(outlier_inds), 2)],
+        SIMPLIFY=FALSE)
+    outlier_ts = unlist(outlier_ts)
+}
 
-plot(diffs, col='steelblue3', lwd=2, xlim=c(6.5,7))
-points(time(tm)[outlier_inds+1], diffs[outlier_inds], col='orange', pch=20, cex=2)
+# plot(diffs, col='steelblue3', lwd=2)#, xlim=c(6.5,7))
+# points(time(tm)[outlier_ts+1], diffs[outlier_ts], col='orange', pch=20, cex=2)
+plot(tm, col='darkgreen', lwd=2, xaxt='n')#, xlim=c(6.5,7))
+points(time(tm)[outlier_ts], tm[outlier_ts], col='orange', pch=20, cex=2)
+
+# plot(diffs, col='steelblue3', lwd=2, xlim=c(6.5,7))
+# points(time(tm)[outlier_inds+1], diffs[outlier_inds], col='orange', pch=20, cex=2)
 plot(tm, col='darkgreen', lwd=2, xaxt='n', xlim=c(6.5,7))
-points(time(tm)[outlier_inds], tm[outlier_inds], col='orange', pch=20, cex=2)
+points(time(tm)[outlier_ts], tm[outlier_ts], col='orange', pch=20, cex=2)
 
-plot(diffs, col='steelblue3', lwd=2, xlim=c(17,18))
-points(time(tm)[outlier_inds+1], diffs[outlier_inds], col='orange', pch=20, cex=2)
+# plot(diffs, col='steelblue3', lwd=2, xlim=c(17,18))
+# points(time(tm)[outlier_inds+1], diffs[outlier_inds], col='orange', pch=20, cex=2)
 plot(tm, col='darkgreen', lwd=2, xaxt='n', xlim=c(17,18))
-points(time(tm)[outlier_inds], tm[outlier_inds], col='orange', pch=20, cex=2)
+points(time(tm)[outlier_ts], tm[outlier_ts], col='orange', pch=20, cex=2)
 
-plot(diffs, col='steelblue3', lwd=2, xlim=c(27,28))
-points(time(tm)[outlier_inds+1], diffs[outlier_inds], col='orange', pch=20, cex=2)
+# plot(diffs, col='steelblue3', lwd=2, xlim=c(27,28))
+# points(time(tm)[outlier_inds+1], diffs[outlier_inds], col='orange', pch=20, cex=2)
 plot(tm, col='darkgreen', lwd=2, xaxt='n', xlim=c(27,28))
-points(time(tm)[outlier_inds], tm[outlier_inds], col='orange', pch=20, cex=2)
+points(time(tm)[outlier_ts], tm[outlier_ts], col='orange', pch=20, cex=2)
+
+# testing left- and right-border outlier cutoffs ####
+#also pos/neg initial jump (relative to first non-outlier jump, etc.)
+
+library(imputeTS)
+setwd('/home/mike/Dropbox/streampulse/data/NC_download/')
+site_files = dir()
+d_chili = read.csv(site_files[1], stringsAsFactors=FALSE,
+    colClasses=c('DateTime_UTC'='POSIXct'))
+sitevars = unique(d_chili$variable)
+
+#mess with input series here
+v=1
+d = d_chili[d_chili$variable == sitevars[v],]
+# d = d[d$DateTime_UTC >= as.POSIXct('2016-10-05 17:45:00') & d$DateTime_UTC <
+#         as.POSIXct('2016-10-11 17:45:00'),]
+
+#get xvals from previous plot
+library(plotrix)
+loc = locator()
+locg = rescale(c(1,loc$x[1],loc$x[2],max(time(tm))), c(1,nrow(d)))
+loc2 = locator()
+locg = rescale(c(1,loc2$x[1],loc2$x[2],max(time(tm))), c(1,nrow(d)))
+d = d[round(locg[2]):round(locg[3]),]
+
+rm(list=c('sd_scaler','big_jump_prop','pos_jumps','neg_jumps','outlier_inds','outlier_ts',
+    'posNeg_jump_pairs','lr','diffs'))
+
+
+#change values
+# derp = which(d$DateTime_UTC %in% chili)
+# d[derp,]
+# d$value[derp] = 110
+
+tm = ts(d$value, deltat = 1/96)
+tm = na.seadec(tm, algorithm='interpolation')
+
+plot(tm, col='darkgreen', lwd=2, xaxt='n')#, xlim=c(6.5,7))
+
+#real stuff
+diffs = diff(tm)
+# x11()
+par(mfcol=c(3,1), mar=c(0,0,0,0), oma=c(5,5,0,0))
+plot(tm, col='darkgreen', lwd=2, xaxt='n')
+mtext('var', 2, 3, outer=FALSE, font=2)
+# par(new=TRUE)
+plot(diffs, col='steelblue3', lwd=2)
+mtext('diff', 2, 3, outer=FALSE, font=2)
+mtext('day', 1, 3, font=2)
+
+u = mean(diffs, na.rm=TRUE)
+sd = sd(diffs, na.rm=TRUE)
+
+sd_scaler = 1.8
+big_jump_prop = Inf
+while(big_jump_prop > 0.03){
+    sd_scaler = sd_scaler + 0.2
+    abline(h=c(-sd_scaler*sd, sd_scaler*sd), lty=3, lwd=2, col='gray60')
+    big_jump_prop = sum(diffs > sd_scaler * sd) / length(diffs)
+}
+
+pos_jumps = which(diffs > sd_scaler * sd)
+neg_jumps = which(diffs < -sd_scaler * sd)
+
+points(time(tm)[pos_jumps], diffs[pos_jumps], col='red', pch=20)
+points(time(tm)[neg_jumps], diffs[neg_jumps], col='purple', pch=20)
+# first_big_jump = min(c(pos_jumps, neg_jumps))
+jump_inds = sort(c(pos_jumps, neg_jumps))
+# jump_inds_s = sort(jump_inds)
+library(accelerometry)
+
+runs = rle2(as.numeric(jump_inds %in% pos_jumps), indices=TRUE)
+# run_start_inds = jump_inds[runs[,'starts']]
+lr = runs[,'lengths'] > 3
+long_runs = runs[lr, 2:3, drop=FALSE]
+# long_run_inds = jump_inds[unique(as.vector(long_runs))]
+
+keep = numeric()
+if(length(long_runs)){
+    for(i in 1:nrow(long_runs)){
+        r = long_runs[i,]
+        j = jump_inds[unique(c(r[1], r[1] + 1, r[2] - 1, r[2]))]
+        if(abs(j[2]-j[1]) < 8 & abs(j[length(j)]-j[length(j)-1]) < 9) next
+        t = time(tm)[j + 1] #+1 to convert from diff indices to original ts indices
+        left_jump_interv = t[2] - t[1]
+        right_jump_interv = t[length(t)] - t[length(t)-1]
+        not_outlier = which.min(c(left_jump_interv, right_jump_interv))
+        keep = append(keep, r[-not_outlier])
+    }
+}
+
+posNeg_jump_pairs = sort(unique(c(keep, as.vector(runs[!lr,2:3]))))
+outlier_inds = jump_inds[posNeg_jump_pairs]
+points(time(tm)[outlier_inds], diffs[outlier_inds], col='orange', pch=20, cex=2)
+
+# inds = outlier_inds
+# inds = multijumps
+# inds=inds[1:2,]
+seq_map = function(inds, x_advance=0, y_advance=0){
+    seq_list = mapply(function(x, y){ seq(x+x_advance, y+y_advance, 1) },
+        # inds[seq(1, length(inds), 2)],
+        # inds[seq(2, length(inds), 2)],
+        inds[,1], inds[,2],
+        SIMPLIFY=FALSE)
+    seq_vec = unlist(seq_list)
+
+    return(seq_vec)
+}
+# if(length(outlier_inds) == 1){
+#     if(length(tm) - outlier_inds <= 15){
+#         outlier_ts = seq(outlier_inds+1, length(tm), 1)
+#     } else {
+#         if(outlier_inds - 1 <= 15){
+#             outlier_ts = seq(1, outlier_inds, 1)
+#         } else {
+#             outlier_ts = NULL
+#         }
+#     }
+
+n_outlier_pieces = Inf
+counter = 0
+if(length(outlier_inds) == 1){
+    outlier_ts = NULL
+} else {
+    while(length(outlier_inds) > 1 & n_outlier_pieces > 50 & counter < 5){
+        outdif = diff(outlier_inds)
+        rm_multjump = rm_oneway = NULL
+
+        short_jumps = outdif < 15
+        if(!short_jumps[1]){
+            outlier_inds = outlier_inds[-1]
+            counter = counter + 1
+            next
+        }
+
+        jump_runs = rle2(as.numeric(short_jumps), indices=TRUE,
+            return.list=FALSE)
+        multijumps = jump_runs[jump_runs[,'lengths'] > 1, 2:3, drop=FALSE]
+        if(length(multijumps)){
+            multijumps = multijumps[short_jumps[multijumps[,'starts']], ,
+                drop=FALSE]
+            rm_multjump = seq_map(multijumps, y_advance=1)
+            # outlier_inds = outlier_inds[-rm_multjump]
+        }
+
+        big_outdif = outdif > 15
+        if(big_outdif[length(big_outdif)]){
+            outlier_inds = outlier_inds[-length(outlier_inds)]
+            counter = counter + 1
+            next
+        }
+
+        if(all(big_outdif)){
+            outlier_ts = NULL
+            break
+        } else {
+        # if(big_outdif[1]){
+            # rm_oneway = outlier_inds[1]
+        # } else {
+            same_sign_runs = rle2(as.numeric(big_outdif), indices=TRUE,
+                return.list=TRUE)
+
+            if(length(same_sign_runs)){
+                l = same_sign_runs$lengths
+                one_way_jumps = numeric()
+                for(i in 1:length(l)){
+                    if(l[i] > 1){
+                        s = same_sign_runs$starts[i]
+                        one_way_jumps = append(one_way_jumps,
+                            seq(s, s + (l[i] - 2), 1))
+                            # same_sign_runs$starts[i]:(i + (l[i] - 2))])
+                    }
+                }
+            }
+
+            # if(same_sign_runs$lengths > 1)
+            # one_way_jumps = same_sign_runs$starts[]
+            if(length(one_way_jumps)){
+                one_way_jumps = one_way_jumps[big_outdif[one_way_jumps]]
+                outdif_filt = outdif
+                outdif_filt[-one_way_jumps] = 0
+                rm_oneway = which(outdif_filt > 0) + 1
+                # rm_oneway = which.max(outdif_filt) + 1
+
+                # if(!all(big_outdiff)){
+                    # outlier_inds = outlier_inds[-1]
+                # } else {
+                    # outlier_ts = NULL
+                # }
+            }
+        }
+        removals = unique(c(rm_multjump, rm_oneway))
+        if(!is.null(removals)){
+            outlier_inds = outlier_inds[-removals]
+        }
+
+        if(length(outlier_inds) %% 2 == 0){
+            outlier_inds = matrix(outlier_inds, ncol=2, byrow=TRUE)
+            outlier_ts = seq_map(outlier_inds, x_advance=1)
+        } else {
+            smallest_diff = which.min(abs(diffs[outlier_inds]))
+            outlier_inds = outlier_inds[-smallest_diff]
+            counter = counter + 1
+            next
+        }
+
+
+
+        # } else {
+            # min_outl = which.min(abs(diffs[outlier_inds]))
+            # outlier_inds = outlier_inds[-min_outl]
+        # }
+        # outlier_inds[which(rle2(outdif)$lengths == 2) + 1]
+        # rle(sign(diff(outlier_inds, differences=2)))$lengths
+        # rm_dd = ifelse(dd[1] - dd[length(dd)] > 0, 1, length(outlier_inds))
+        # outlier_inds = outlier_inds[-rm_dd]
+        # outlier_ts = seq_map(outlier_inds, x_advance=1)
+        # }
+        n_outlier_pieces = length(outlier_ts)
+        counter = counter + 1
+    }
+}
+if(n_outlier_pieces > 50){
+    outlier_ts = NULL
+    print('argh2')
+}
+#         if(rm_dd == 1 & outlier_inds[1] <= 15){
+#             left_bounded_outlier_seq = seq(1, outlier_inds[1], 1)
+#             outlier_inds = outlier_inds[-1]
+#             outlier_ts = seq_map(outlier_inds)
+#             outlier_ts = c(outlier_ts, left_bounded_outlier_seq)
+#         } else {
+#             right_bounded_outlier_seq = seq(outlier_inds[length(outlier_inds)]+1,
+#                 length(tm), 1)
+#             if(rm_dd == 2 & length(right_bounded_outlier_seq) <= 15) {
+#                 outlier_inds = outlier_inds[-length(outlier_inds)]
+#                 outlier_ts = seq_map(outlier_inds)
+#                 outlier_ts = c(outlier_ts, right_bounded_outlier_seq)
+#             }
+#         }
+#     }
+# }
+# if(length(outlier_ts) > 50) outlier_ts = NULL
+# outlier_ts = outlier_inds
+# plot(diffs, col='steelblue3', lwd=2)#, xlim=c(6.5,7))
+# points(time(tm)[outlier_ts+1], diffs[outlier_ts], col='orange', pch=20, cex=2)
+plot(tm, col='darkgreen', lwd=2, xaxt='n')#, xlim=c(6.5,7))
+points(time(tm)[outlier_ts], tm[outlier_ts], col='orange', pch=20, cex=2)
+(chili = d$DateTime_UTC[outlier_ts])
+print(paste('sd_scaler =', sd_scaler))
+print(paste('big_jump_prop =', big_jump_prop))
+
+# plot(tm, col='darkgreen', lwd=2, xaxt='n', xlim=c(16.5,16.8))
+# points(time(tm)[outlier_ts], tm[outlier_ts], col='orange', pch=20, cex=2)
+# axis(1, time(tm)[outlier_ts], tm[outlier_ts], las=2)
+
+
+# plot(tm, col='darkgreen', lwd=2, xaxt='n', xlim=c(21.5,22.5))
+# # plot(tm, col='darkgreen', lwd=2, xaxt='n', xlim=c(.8,1.5))
+# points(time(tm)[outlier_ts], tm[outlier_ts], col='orange', pch=20, cex=2)
+# axis(1, time(tm), d$DateTime_UTC, las=2)
