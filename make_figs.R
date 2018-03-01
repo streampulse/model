469e2f26b29db71397263b2e07d808b5b414eaf2
Mike Vlah <vlahm13@gmail.com>
2018-02-28 19:17:18 -0800

    updated make_figs setup post-packaging

--- make_figs.R
+++ make_figs.R
@@ -1,35 +1,132 @@
 rm(list=ls()); cat('\014') #clear environment and console
 
 #setup ####
-#install packages from CRAN if necessary
-package_list <- c('coda','plyr','dplyr','httr','jsonlite','R2jags',
-    'tidyr','imputeTS','accelerometry','MetaboPlots','zoo',
-    'geosphere','readr','tibble') #geoknife
-new_packages <- package_list[!(package_list %in% installed.packages()[,"Package"])]
-if(length(new_packages)) install.packages(new_packages)
+# library(devtools) #devtools is necessary for installing packages from GitHub.
+# install_github('streampulse/StreamPULSE', dependencies=TRUE)
+# install.packages('streamMetabolizer', dependencies=TRUE,
+# repos=c('https://owi.usgs.gov/R','https://cran.rstudio.com'))
 
-#install streamMetabolizer from github (stable version)
-if (!require("streamMetabolizer")) {
-    if (!require("devtools")) install.packages('devtools', repos="http://cran.rstudio.com/")
-    library(devtools)
-    # install_github('USGS-R/streamMetabolizer', ref='develop') #install from github
-    #install specific stable release on github. check the site to find out
-    #if the library() text doesnt alert you:
-    # https://github.com/USGS-R/streamMetabolizer/releases
-    install_github('USGS-R/streamMetabolizer@v0.10.8')
-    detach('package:devtools', unload=TRUE)
+library(StreamPULSE)
+library(streamMetabolizer)
+library(ks)
+library(RColorBrewer)
+
+processing_func = function (ts, date_var, gpp_var, er_var) {
+    ts[!is.na(ts[, gpp_var]) & ts[, gpp_var] < 0 | !is.na(ts[,
+        gpp_var]) & ts[, gpp_var] > 100, gpp_var] <- NA
+    ts[!is.na(ts[, er_var]) & ts[, er_var] > 0, "er_var"] <- NA
+    if('tbl' %in% class(ts[,date_var])){
+        full_dates = as.data.frame(ts[,date_var])
+        colnames(full_dates) = 'Date'
+        colnames(ts)[which(colnames(ts) == 'date')] = 'Date'
+    } else {
+        ts_date = ts[,date_var]
+        ts$Date <- format(ts_date, "%Y-%m-%d")
+        full_dates <- setNames(data.frame(
+            seq(from = as.Date(paste(min(format(ts[, date_var], "%Y")),
+                "-01-01", sep = "")),
+                to = as.Date(paste(max(format(ts[,
+                    date_var], "%Y")), "-12-31", sep = "")), by = 1)), "Date")
+    }
+    ts_full <- merge(full_dates, ts, by = c("Date"), all = TRUE)
+    ts_full$Year <- as.numeric(format(ts_full[, "Date"], "%Y"))
+    ts_full$DOY <- as.numeric(format(ts_full[, "Date"], "%j"))
+    ts_full$NPP <- ts_full[, gpp_var] + ts_full[, er_var]
+    return(ts_full)
 }
-
-#make sure streamMetabolizer is up to date
-# update.packages(oldPkgs=c("streamMetabolizer","unitted"),
-#     dependencies=TRUE, repos=c("https://owi.usgs.gov/R",
-#         "https://cran.rstudio.com"))
-
-#load all packages
-for(i in c(package_list)) library(i, character.only=TRUE)
-
-source('~/git/streampulse/model/gapfill_functions.R')
-source('~/git/streampulse/model/sp_functions.R')
+season_ts_func = function (ts_full, gpp_var, er_var){
+    avg_trajectory <- aggregate(ts_full, by = list(ts_full[,
+        "DOY"]), FUN = mean, na.rm = TRUE)
+    sd_trajectory <- aggregate(ts_full, by = list(ts_full[,
+        "DOY"]), FUN = sd, na.rm = TRUE)
+    llim <- min(c(avg_trajectory[, er_var],
+        avg_trajectory[, gpp_var]), na.rm = T)
+    ulim <- max(c(avg_trajectory[, er_var],
+        avg_trajectory[, gpp_var]), na.rm = T)
+    # plot(avg_trajectory[, gpp_var],
+    plot(avg_trajectory$DOY, avg_trajectory[, gpp_var],
+        type = "l", col = "red", xlab = '', ylab = expression(paste("gO"[2] *
+                " m"^"-2" * " d"^"-1")), ylim = c(llim, ulim), lwd = 2,
+        xaxt='n', xlim=c(1, 366))
+    month_labs = month.abb
+    month_labs[seq(2, 12, 2)] = ''
+    axis(1, seq(1, 365, length.out=12), month_labs)
+    # t = avg_trajectory$Date
+    # yearstarts = match(unique(substr(t,1,4)), substr(t,1,4))
+    # monthstarts = match(unique(substr(t,1,7)), substr(t,1,7))
+    # axis(1, yearstarts, rep('', length(yearstarts)), lwd.ticks=2, tck=-0.05)
+    # axis(1, yearstarts, substr(t[yearstarts],1,4), line=1, tick=FALSE)
+    # month_abbs = month.abb[as.numeric(substr(t[monthstarts],6,7))]
+    # axis(1, monthstarts[-1], month_abbs[-1])
+    lines(avg_trajectory[, "DOY"], avg_trajectory[, er_var],
+        # lines(avg_trajectory[, er_var],
+        col = "steelblue", lwd = 2)
+    lines(avg_trajectory[, "DOY"], avg_trajectory[, "NPP"],
+        # lines(avg_trajectory$NPP,
+        col = "grey60", lwd = 2)
+    abline(h = 0)
+    legend("topleft", inset = c(0.1, -0.15), ncol = 3, xpd = TRUE,
+        c("GPP", "NEP", "ER"), bty = "n", lty = c(1, 1, 1),
+        lwd = 2, col = c("red", "grey60", "steelblue"))
+}
+cumulative_func = function (ts_full, gpp_var, er_var){
+    na_rm <- na.omit(ts_full[, c("Year", "DOY", gpp_var, er_var,
+        "NPP")])
+    na_rm$csum_gpp <- ave(na_rm[, gpp_var], na_rm[, "Year"],
+        FUN = cumsum)
+    na_rm$csum_er <- ave(na_rm[, er_var], na_rm[, "Year"], FUN = cumsum)
+    na_rm$csum_npp <- ave(na_rm[, "NPP"], na_rm[, "Year"], FUN = cumsum)
+    lim <- max(abs(na_rm[, c("csum_gpp", "csum_er")]))
+    pal <- rev(brewer.pal(7, "Spectral"))
+    cols <- setNames(data.frame(unique(na_rm[, "Year"]), pal[1:length(unique(na_rm[,
+        "Year"]))]), c("Year", "color"))
+    csum_merge <- merge(na_rm, cols, by = "Year", type = "left")
+    plot(csum_merge$DOY, csum_merge[, "csum_gpp"], pch = 20,
+        cex = 1.5, col = paste(csum_merge[, "color"]), ylim = c(0,
+            lim), xaxt = "n", xlim=c(0, 366),  ylab = "Cumulative GPP",
+        type='p')
+    legend("topleft", paste(c(cols[, "Year"])), lwd = c(1, 1),
+        col = paste(cols[, "color"]), cex = 0.9)
+    plot(csum_merge$DOY, csum_merge[, "csum_er"], pch = 20,
+        cex = 1.5, col = paste(csum_merge[, "color"]), ylim = c(-lim,
+            0), xaxt = "n", xlim=c(0, 366), ylab = "Cumulative ER",
+        type='p')
+    plot(csum_merge$DOY, csum_merge[, "csum_npp"], pch = 20,
+        cex = 1.5, col = paste(csum_merge[, "color"]), ylim = c(-lim,
+            lim), ylab = "Cumulative NPP", xlim=c(0, 366),
+        xlab = '', type='p', xaxt='n')
+    month_labs = month.abb
+    month_labs[seq(2, 12, 2)] = ''
+    axis(1, seq(1, 365, length.out=12), month_labs)
+    abline(h = 0, col = "grey60", lty = 2)
+}
+kernel_func = function (ts_full, gpp_var, er_var){
+    kernel <- kde(na.omit(ts_full[, c(gpp_var, er_var)]))
+    k_lims <- max(abs(c(min(ts_full[, er_var], na.rm = TRUE),
+        max(ts_full[, gpp_var], na.rm = TRUE))))
+    plot(kernel, xlab = expression(paste("GPP (gO"[2] * " m"^"-2" *
+            " d"^"-1" * ")")), ylab = expression(paste("ER (gO"[2] *
+                    " m"^"-2" * " d"^"-1" * ")")), ylim = c(-k_lims, 0),
+        xlim = c(0, k_lims), display = "filled.contour2", col = c(NA,
+            "gray80", "gray60", "gray40"))
+    abline(0, -1)
+    legend("topright", c("75%", "50%", "25%"), bty = "n", lty = c(1,
+        1, 1), lwd = 2, col = c("gray80", "gray60", "gray40"))
+}
+diag_plots = function (ts, date_var, gpp_var, er_var){
+    ts_full <- processing_func(ts, date_var, gpp_var, er_var)
+    layout(matrix(c(1, 1, 3, 1, 1, 4, 2, 2, 5), 3, 3, byrow = TRUE),
+        widths = c(1, 1, 2))
+    par(cex = 0.6)
+    par(mar = c(3, 4, 0.1, 0.1), oma = c(3, 0.5, 0.5, 0.5))
+    par(tcl = -0.25)
+    par(mgp = c(2, 0.6, 0))
+    kernel_func(ts_full, gpp_var, er_var)
+    par(mar = c(0, 4, 2, 0.1))
+    season_ts_func(ts_full, gpp_var, er_var)
+    par(mar = c(0, 4, 0, 0.1))
+    cumulative_func(ts_full, gpp_var, er_var)
+}
 
 #choices ####
 
@@ -48,7 +145,7 @@ site_code = "SE_AbiskoM16"; start_date = "2016-05-27"; end_date = '2016-10-30'
 site_code = "SE_AbiskoM17"; start_date = "2016-06-15"; end_date = '2016-09-08'
 site_code = "SE_M18"; start_date = "2016-06-08"; end_date = '2016-10-22'
 site_code = "SE_M6"; start_date = "2016-06-08"; end_date = '2016-10-22'
-site_code = "PR_QS"; start_date = "2015-01-01"; end_date = '2015-12-31'#ed "2017-12-20"
+site_code = "PR_QS"; start_date = "2015-01-01"; end_date = '2015-11-30'#ed "2017-12-20"
 site_code = "RI_CorkBrk"; start_date = "2016-01-01"; end_date = '2016-12-31'#ed "2017-01-03", sd=2014-06-23
 #site_code = "CT_BUNN"; start_date = "2015-05-20"; end_date = '2016-05-20'#ed "2016-11-15"
 #site_code = "CT_HUBB"; start_date = "2015-05-20"; end_date = '2016-05-20'#ed "2016-11-11"
