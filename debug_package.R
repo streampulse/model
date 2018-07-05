dab5332abc3321ec2bf70f739ac52c322e750650
Mike Vlah <vlahm13@gmail.com>
2018-07-05 16:51:34 -0700

    small changes to debug_package script

--- debug_package.R
+++ debug_package.R
@@ -14,9 +14,13 @@ setwd('~/git/streampulse/model/StreamPULSE/R/')
 fs = list.files()
 for(i in fs) source(i)
 
-#now go to hax/R/package_write.R if necessary
+#now go to hax/R/R_packages/package_write.R if ready to test for reals
+library(StreamPULSE)
+library(streamMetabolizer)
 
-x = request_data('NC_Eno', '2017-02-01', '2017-02-15',
+
+#testing model comparison and API upload infrastructure ####
+x = request_data('NC_Eno', '2017-01-19', '2017-02-05',
     token='901bb597c597e3237d00')
 
 x$specs
@@ -29,8 +33,9 @@ o = fit_metabolism(y)
 # saveRDS(fff, '~/Desktop/test_fit.rds')
 # saveRDS(ppp, '~/Desktop/test_pred.rds')
 
-
 model_fit = readRDS('~/Desktop/test_fit.rds')
 predictions = readRDS('~/Desktop/test_pred.rds')
+mod_startyr = '2017'
 d = y
 output = list(predictions=predictions, fit=model_fit)
+deets = extract_model_details(model_fit, predictions, d$specs)
